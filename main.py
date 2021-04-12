add158c672f6d3f2bb8b760c9e61b2a0f504a063
fistadev <fabiofistarol@gmail.com>
2021-04-12 11:39:13 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def scraper():
+    # your code here
+
+
+def preprocessing(csv_path):
+    # your code here
+    return dataframe
+
+
+def analyse(df):
+    # your code here
+
+
+def main():
+    scraper()
+    df = preprocessing(csv_path)
+    analyse(df)
+
+
+if __name__ == "__main__":
+    main()
